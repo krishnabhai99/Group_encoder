08a4e959f6d4187e1bf3a45a32c3edbcff12ce71
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2025-01-18 14:58:30 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,12 +7,12 @@ class Config(object):
     # sucks Dude
     APP_ID = int(get_config("APP_ID", "26387127"))
     API_HASH = get_config("API_HASH", "19718ab7acd97d0f71ada2807ddfe47a")
-    LOG_CHANNEL = get_config("LOG_CHANNEL", "NEW_ERA_ENCODING_GP")
+    LOG_CHANNEL = get_config("LOG_CHANNEL", "krishna_bios")
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
      # Get these values from my.telegram.org
     AUTH_USERS = [5446367898]
 # array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🌹
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "7257386978:AAGoHTntoUEbi2A3emmIu3R5zHGrXlydpLM")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "7257386978:AAH28bDIlS-2mTnzOonEAz-HxU_ZOwFuhlA")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
